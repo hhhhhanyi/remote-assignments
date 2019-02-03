05065ec0c53fb7be625ddb8a911df304072a2612
hhhhhanyi <45988367+hhhhhanyi@users.noreply.github.com>
2019-02-03 12:24:51 +0800

    Update app.js

--- week-4/public/app.js
+++ week-4/public/app.js
@@ -5,7 +5,7 @@ Complete the function below to show a delayed result in console.
 function delayedResult(n1, n2, delayTime, callback){
   // your code here
   let sum = n1 + n2;
-  setTimeout(callback(sum), delayTime);
+  window.setTimeout(function(){callback(sum);}, delayTime);
 }
 
 delayedResult(4, 5, 3000, function(result){
@@ -23,25 +23,31 @@ Assignment 2: Callback Function and Advanced HTML DOM
 Complete the function below to make AJAX call to an URL by GET method,
  and show the response data in the page. You may render UI just in any simple and human-friendly style.
 */
-$(document).ready(function() {
 
   function ajax(src, callback){
     // your code here
-    $.getJSON(src,callback);
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', src);
+    xhr.onreadystatechange = function () {
+      if(xhr.readyState === 4 && xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        render(data);
+      }
+    };
+    xhr.send();
   }
 
   function render(data){
     // your code here.
     // document.createElement() and appendChild() methods are preferred.
-    $.each(data,function(i,product) {
-      var list = document.createElement("li");
-      list.innerHTML = "商品名稱：" + product.name + "<br>價錢：" + product.price + "<br>描述：" + product.description;
+    for (let i=0; i<data.length; i += 1) {
+      let list = document.createElement("li");
+      list.innerHTML = "商品名稱：" + data[i].name + "<br>價錢：" + data[i].price + "<br>描述：" + data[i].description;
       document.getElementById("demo").appendChild(list);
-    });
+    }
   }
 
   ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){
     render(response);
   });
   // you should get product information in JSON format and render data in the page
-});
